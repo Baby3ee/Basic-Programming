6bc00e403210d10f1ea8cf52853b55fb5ea1f8e8
Yuttana ChuanChuChai <646051200129@mail.rmutk.ac.th>
2024-07-23 11:50:28 +0700

    เรียน Function

---
+++ EP2/Test.ts
@@ -0,0 +1,5 @@
+function remove(a: number, b: number) {
+    return a - b
+}
+
+console.log(remove(5,10))
---
+++ EP2/Test1.ts
@@ -0,0 +1,5 @@
+function multiply(a:number, b:number, c:number) {
+    return a * b * c
+}
+
+console.log(multiply(2,2,2))
