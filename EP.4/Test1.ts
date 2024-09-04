04a904cec5ecbcfc60c7bfe745b787bbbc5a046f
Baby3ee <beerawiwan1626@gmail.com>
2024-09-04 11:04:35 +0700

    homework

--- EP.4/Test1.ts
+++ EP.4/Test1.ts
@@ -1,48 +1,48 @@
-const str: string = `สวัสดี` // ` เรียกว่า Template strinng
+// const str: string = `สวัสดี` // ` เรียกว่า Template strinng
 
-console.log(str.charAt(1))
+// console.log(str.charAt(1))
 
-const fName: string = 'นายทดสอบ'
-const lName: string = 'นามสกุล'
+// const fName: string = 'นายทดสอบ'
+// const lName: string = 'นามสกุล'
 
-const fullName: string = fName + ' ' + lName
-console.log('แบบใหม่'fullName)
+// const fullName: string = fName + ' ' + lName
+// console.log('แบบใหม่'fullName)
 
-console.log(fName.concat(" ", lName)) //การนำคำมาเชื่อมต่อกัน
+// console.log(fName.concat(" ", lName)) //การนำคำมาเชื่อมต่อกัน
 
-const str1: string = "TypeScript"
+// const str1: string = "TypeScript"
 
-console.log(str1.indexOf('p')) //หาตัว p ในคำ
+// console.log(str1.indexOf('p')) //หาตัว p ในคำ
 
-const str2: string = "TypeScript"
+// const str2: string = "TypeScript"
 
-console.log(str2.lastIndexOf('p')) //หาตัว p ตัวสุดท้าย ในคำ
+// console.log(str2.lastIndexOf('p')) //หาตัว p ตัวสุดท้าย ในคำ
 
 
-const str3: string = "TypeScript"
+// const str3: string = "TypeScript"
 
-console.log(str3.includes('p')) //จะบอกว่ามีตัวอักษรในคำนี้หรือไม่ ตอบแค่ จริงกับไม่จริง
+// console.log(str3.includes('p')) //จะบอกว่ามีตัวอักษรในคำนี้หรือไม่ ตอบแค่ จริงกับไม่จริง
 
-const str4: string = "นางสาวทดสอบ นามสกุล"
+// const str4: string = "นางสาวทดสอบ นามสกุล"
 
-console.log(str4.includes('นางสาว')) //จะบอกว่ามีตัวอักษรในคำนี้หรือไม่ ตอบแค่ จริงกับไม่จริง
+// console.log(str4.includes('นางสาว')) //จะบอกว่ามีตัวอักษรในคำนี้หรือไม่ ตอบแค่ จริงกับไม่จริง
 
-const str5: string = 'Hello my Name is John I so happy'
+// const str5: string = 'Hello my Name is John I so happy'
 
-console.log(str5.slice(str5.indexOf('John'), str5.indexOf('John') + "john".length)) //ใช้ตัดคำที่เราต้องการ ตำแหน่งสุดท้าย +1
+// console.log(str5.slice(str5.indexOf('John'), str5.indexOf('John') + "john".length)) //ใช้ตัดคำที่เราต้องการ ตำแหน่งสุดท้าย +1
 
-const str6: string = "61,325.33:61,325.32:61,325.31:,325.30"
+// const str6: string = "61,325.33:61,325.32:61,325.31:,325.30"
 
-console.log(str6.split(':'))
+// console.log(str6.split(':'))
 
-const str: string = 'ยายแล่ม เมื่อตอนสาว ๆ'
+// const str: string = 'ยายแล่ม เมื่อตอนสาว ๆ'
 
-console.log(str.replace('ยายแล่ม','อ้นจี้้')) //คือการแทนที่เฉพาะคำแรกที่เจอ
+// console.log(str.replace('ยายแล่ม','อ้นจี้้')) //คือการแทนที่เฉพาะคำแรกที่เจอ
 
-const str: string = 'ยายแล่ม เมื่อตอนสาว ๆ'
+// const str: string = 'ยายแล่ม เมื่อตอนสาว ๆ'
 
-console.log(str.replaceAll('ยายแล่ม','อ้นจี้้')) //คือการแทนที่ทั้งหมดที่มี
+// console.log(str.replaceAll('ยายแล่ม','อ้นจี้้')) //คือการแทนที่ทั้งหมดที่มี
 
-const str: string = 'นายนอนน้อย  แต่นอนนะ'
+// const str: string = 'นายนอนน้อย  แต่นอนนะ'
 
-console.log(str.replace('นาย', ' ')) // เอานายออกด้วยการใช้replace
+// console.log(str.replace('นาย', ' ')) // เอานายออกด้วยการใช้replace
---
+++ EP6/homeWork.ts
@@ -0,0 +1,13 @@
+const  me =  {
+    Name : "Degoo Mousi",
+    Address : 24,
+    Moo : 4,
+    Road : "Littlewing",
+    Tumbon : "Wingging",
+    District : "Wing",
+    Province : "Londone",
+    PostalCode : 12048,
+    Telephone : 2-256-3659-9,
+    Mobilephone : 95-788-9550,
+    Email :"DeegooMousi2679@gmail.com"
+}
